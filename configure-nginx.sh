c436c6f50a5c3e883c0b81df74b4ba4f486f44e4
DesertEMT <149753133+DesertEMT@users.noreply.github.com>
2023-11-07 00:11:44 -0800

    Update configure-nginx.sh

--- configure-nginx.sh
+++ configure-nginx.sh
@@ -7,4 +7,7 @@ sudo apt-get update
 sudo apt-get install -y nginx
 
 # Set the home page.
-echo "<html><body><h2>Hello and welcome to my test Virtual Machine made in Azure! My name is probably $(hostname).</h2></body></html>" | sudo tee -a /var/www/html/index.html
+echo "<html><body><h2>Hello and welcome to my web page! I'm Michael.<br>
+I'm currently taking CS135 at CBC and also work in corporate IT.<br>
+Before going back to school for computer science I worked in law enforcement for a decade. In my free time, I like to backpack and mountain climb.<br>
+You're reading this from a website I created on an Azure Virtual Machine named $(hostname).</h2></body></html>" | sudo tee -a /var/www/html/index.html
